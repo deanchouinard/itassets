21f730772d905bab1fb6ba760e6bc2287379bdef
Dean Chouinard <deanchouinard@gmail.com>
2011-06-04 22:19:44 -0400

    Upgraded to Rails3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-	layout "companies"
+  protect_from_forgery
+  
+  
 	before_filter :authorize, :except => :login
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+#    protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
-	
+    # Scrub sensitive parameters from your log
+    # filter_parameter_logging :password
+
 protected
 	def authorize
 		unless User.find_by_id(session[:user_id])
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_itassets_session',
-  :secret      => '0ed6561bb4bbae19ed1c8abc239a8641a58e89df5fc1a9500daffda8af4fce1f722c4ea67a07e9c12a84e013cf27b4736cba6d7c05c757b524792e8ffab42c23'
-}
+Itassets::Application.config.session_store :cookie_store, :key => '_itassets_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Itassets::Application.config.session_store :active_record_store
