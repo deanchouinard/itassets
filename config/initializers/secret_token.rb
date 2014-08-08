c46ce244c877d6cd5626a29a07e9f8e70a0fe2d2
Dean Chouinard <deanchouinard@gmail.com>
2014-08-08 11:02:16 -0400

    use secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Itassets::Application.config.secret_token = '5874a6a64eae41e8dd923e01d28f4bd945d272a085efb93d32f9bc82ea01bdbe479f3fb778dc58b9cfc321834ba0ff6a72e67fd80a29d3b2fe7594d99aec4383'
